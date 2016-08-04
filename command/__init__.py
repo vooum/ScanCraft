06271fc8c31660ca5e51f685ab4db7b6853d9ac2
vooum <vooum@qq.com>
2016-08-04 19:56:55 +0800

    update .gitignore

---
+++ command/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from .init_NMSSMTools import *
+from . import read
+#     chisq_mh = chi2(value_mh,mh)
+def chi2(mu, data): 
+    return (mu-data[0])**2/(data[1]**2+data[2]**2)
+mh =[125.7, 0.4, 1.5]
+bsg=[3.43, 0.21, 0.3]
+bmu=[2.9, 0.7, 0.38]
+omg=[0.1187,0.0017,0.012]
+gm2=[28.7,8.0,2.0]
+
