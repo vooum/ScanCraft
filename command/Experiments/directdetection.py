9f0620789a773526e028d0b2f0fbcd512520e0bc
shl-office <vooum@qq.com>
2017-04-03 15:12:08 +0800

    interplate

--- command/Experiments/directdetection.py
+++ command/Experiments/directdetection.py
@@ -12,6 +12,6 @@ class DirectDetection:
             exit('Entered mass value too small. Out of the range provided by '+repr(self.exp))
         for i in range(1,self.line.shape[0]):
             if mass < self.line[i,0]: 
-                return self.line[i-1,1]+(self.line[i,1]-self.line[i-1,1])/(self.line[i,0]-self.line[i-1,0])
+                return self.line[i-1,1]+(mass-self.line[i-1,0])*(self.line[i,1]-self.line[i-1,1])/(self.line[i,0]-self.line[i-1,0])
         else:
             exit('Entered mass value too large. Out of the range provided by '+repr(self.exp))
