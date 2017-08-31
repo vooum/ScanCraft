3b75b6a4db75e0b6c0fb175f224df4ce75349f81
shl-office <vooum@qq.com>
2017-08-31 16:36:45 +0800

    seperate_git

--- README.md
+++
@@ -1,8 +0,0 @@
-# ScanCommando
-offer a shell of NMSSMTools to scan by MCMC
-still developing.
-
-HP6300 linked
-
-
-provided by HeYangle
--- command/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-#from .chisqure import *
-#from .darkmatter import *
-#from . import mcmc
-#from .read import *
-#from .NMSSMTools import *
