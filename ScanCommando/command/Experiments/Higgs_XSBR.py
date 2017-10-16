1a23c23d1ad622629a231986c63caefa60c24cd6
shl-office <vooum@qq.com>
2017-10-16 20:56:10 +0800

    modified:   ScanCommando/command/Experiments/Higgs_XSBR.py

--- ScanCommando/command/Experiments/Higgs_XSBR.py
+++ ScanCommando/command/Experiments/Higgs_XSBR.py
@@ -12,7 +12,7 @@ def interp_XS(file_name):
     data=GetCS(os.path.join(os.path.dirname(__file__),'Higgs_XSBR',file_name))
     mass=data[:,0]
     CS=data[:,1]
-    return interp1d(mass,CS,bounds_error=True,fill_value='extrapolate')
+    return interp1d(mass,CS,fill_value='extrapolate')
 
 class Higgs_XS():
     ggF=interp_XS('Higgs_CS_BSM_13TeV_ggF.txt')
