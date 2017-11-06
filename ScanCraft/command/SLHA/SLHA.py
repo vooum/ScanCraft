55291aeb904fb28d940f573af7c62ef563228b76
He Yangle <vooum@qq.com>
2017-11-06 16:15:02 +0800

    SLHA0

---
+++ ScanCraft/command/SLHA/SLHA.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+flat_to_str=lambda L: sum(map(flat_to_str,L),[]) if isinstance(L,(list,tuple)) else [str(L)]
+
+class list_block():
+    def __init__(self,PDG_name_dict):
+        self.PDG_name_dict=PDG_name_dict
+    def __call__(self,number):
+        return self.PDG_name_dict[number]
+            
+class matrix_block():
+    def __init__(self,name):
+        self.name=name
+    def __call__(self,*element):
+        return '_'.join(flat_to_str([self.name,element]))
+        
+
+class SLHA():
+    MNPAR=list_block({3:'tanB'})
+    HMIX=matrix_block('Hmix')
