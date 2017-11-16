5caec3d7b410988623aab115891c7111b6fb04f4
He Yangle <vooum@qq.com>
2017-11-16 21:52:24 +0800

    1116

--- ScanCraft/command/format/SLHA.py
+++ ScanCraft/command/format/SLHA.py
@@ -15,17 +15,17 @@ class matrix_block():
         return '_'.join(flat_to_str([self.name,element]))  #例如self.name=Hmix,element=(3,3) 它们组成列表[Hmix,3,3],
 
 SMINPUTS={1:'ALPHA_EM^-1(MZ)',2:'GF',3:'ALPHA_S(MZ)',
-          4:'MZ',5:'MB',6:'MTOP',7:'MTAU'}
+          4:'MZ',5:'MB',6:'MTOP',7:'MTAU'}        
 MINPAR={3:'tanB',0:'SCALE'}
 EXTPAR={1:'M1',2:'M2',3:'M3',
-        11:'ATOP',12:'ABOTTOM',
-        13:'ATAU',16:'AMUON',
-        31:'L_SELECTRON',32:'L_SMUON',33:'L_STAU',
-        34:'R_SELECTRON',35:'R_SMUON',36:'R_STAU',
+        11:'Atop',12:'Abottom',
+        13:'Atau',16:'Amuon',
+        31:'L1',32:'L2',33:'L3',
+        34:'E1',35:'E2',36:'E3',
         41:'Q1',42:'Q2',43:'Q3',
         44:'U1',45:'U2',46:'U3',
         47:'D1',48:'D2',49:'D3',
-        61:'LAMBDA',62:'KAPPA',63:'ALAMBDA',64:'AKAPPA',
+        61:'Lambda',62:'Kappa',63:'ALambda',64:'AKappa',
         65:'MUEFF',124:'MA_QSTSB'}
 MASS={5:'MB',6:'MTOP',15:'MTAU',23:'MZ',24:'MW',
       25:'H1',35:'H2',45:'H3',36:'A1',46:'A2',37:'C1',
@@ -37,7 +37,7 @@ MASS={5:'MB',6:'MTOP',15:'MTAU',23:'MZ',24:'MW',
       1000015:'~tau_L',2000015:'~tau_R',1000016:'snu_3',1000021:'~g',
       1000022:'N1',1000023:'N2',1000025:'N3',1000035:'N4',1000045:'N5',
       1000024:'ch1',1000037:'ch2',1000039:'gravitino'}
-LOWEN={1:'BR(b->s_gamma)',10:'DMRD',11:'BR(b->s_gamma+Theor_Err)',12:'BR(b->s_gamma-Theor_Err)',
+LOWEN={1:'BR(b->s_gamma)',11:'BR(b->s_gamma+Theor_Err)',12:'BR(b->s_gamma-Theor_Err)',
        2:'DeltaM_d_ps^-1',21:'DeltaM_d+Theor_Err',22:'DeltaM_d-Theor_Err',
        3:'DetaM_s',31:'DeltaM_s+Theor_Err',32:'DeltaM_s-Theor_Err',
        4:'BR(Bs->mu+mu-)',41:'BR(Bs->mu+mu-)+Theor_Err',42:'BR(Bs->mu+mu-)-Theor_Err',
@@ -48,15 +48,15 @@ GAUGE={1:'g1',2:'g2',3:'g3'}
 MSOFT={1:'M1',2:'M2',3:'M3',21:'M_HD',22:'M_HU',31:'M_eL',32:'M_muL',33:'M_tauL',
        34:'M_eR',35:'M_muR',36:'M_tauR',41:'M_q1L',42:'M_q2L',43:'M_q3L',
        44:'M_uR',45:'M_cR',46:'M_tR',47:'M_dR',48:'M_sR',49:'M_bR'}
-NMSSMRUN={1:'LAMBDA',2:'KAPPA',3:'ALAMBDA',4:'AKAPPA',5:'MUEFF',
+NMSSMRUN={1:'Lambda',2:'Kappa',3:'ALambda',4:'AKappa',5:'MUEFF',
           6:'XIF',7:'XIS',8:'MUP',9:'MSP',10:'MS'}
-GUTGAUGE={1:'g1',2:'g2',3:'g3'}
-GUTMSOFT={1:'M1',2:'M2',3:'M3',21:'M_HD',22:'M_HU',31:'M_eL',32:'M_muL',33:'M_tauL',
-          34:'M_eR',35:'M_muR',36:'M_tauR',41:'M_q1L',42:'M_q2L',43:'M_q3L',
-          44:'M_uR',45:'M_cR',46:'M_tR',47:'M_dR',48:'M_sR',49:'M_bR'}
-GUTNMSSMRUN={1:'LAMBDA',2:'KAPPA',3:'ALAMBDA',4:'AKAPPA',5:'MUEFF',
-             6:'XIF',7:'XIS',8:'MUP',9:'MSP',10:'MS',12:'M3H'}
-FINETUNING={1:'PS=MHU',2:'PS=MHD',3:'PS=MS',4:'PS=ALAMBDA',5:'PS=AKAPPA',
+GUTGAUGE={1:'GUTg1',2:'GUTg2',3:'GUTg3'}
+GUTMSOFT={1:'GUT_M1',2:'GUT_M2',3:'GUT_M3',21:'GUTM_HD',22:'GUTM_HU',31:'GUTM_eL',32:'GUTM_muL',33:'GUTM_tauL',
+          34:'GUTM_eR',35:'GUTM_muR',36:'GUTM_tauR',41:'GUTM_q1L',42:'GUTM_q2L',43:'GUTM_q3L',
+          44:'GUTM_uR',45:'GUTM_cR',46:'GUTM_tR',47:'GUTM_dR',48:'GUTM_sR',49:'GUTM_bR'}
+GUTNMSSMRUN={1:'GUT_Lambda',2:'GUT_Kappa',3:'GUT_ALambda',4:'GUT_AKappa',5:'GUT_MUEFF',
+             6:'GUT_XIF',7:'GUT_XIS',8:'GUT_MUP',9:'GUT_MSP',10:'GUT_MS',12:'GUT_M3H'}
+FINETUNING={1:'PS=MHU',2:'PS=MHD',3:'PS=MS',4:'PS=ALambda',5:'PS=AKappa',
             6:'PS=XIF',7:'PS=XIS',8:'PS=MUP',9:'PS=MSP',10:'PS=M3H',
             11:'PS=LAMBDA',12:'PS=KAPPA',13:'PS=HTOP',14:'PS=G',15:'MAX',16:'IMAX'}
 LHCCROSSSECTIONS={11:'VBF/VH->H1->tautau',
@@ -89,7 +89,6 @@ LHCCROSSSECTIONS={11:'VBF/VH->H1->tautau',
                   36:'ggf->H3->ZZ/WW',
                   38:'ggf->H3->gammagamma',
                   310:'ggf->H3->invisible',}
-
 LHCFIT={1:'Hgammagamma',2:'Hff',3:'HVV'}
 
 
@@ -108,9 +107,9 @@ class SLHA():
     FINETUNING=list_block(FINETUNING)
     LHCCROSSSECTIONS=list_block(LHCCROSSSECTIONS)
     LHCFIT=list_block(LHCFIT)
-    NMHMIX=matrix_block('Hmix')
-    NMAMIX=matrix_block('Amix')
-    STOPMIX=matrix_block('~Tmix')
+    NMHMIX=matrix_block('NMHMIX')
+    NMAMIX=matrix_block('NMAMIX')
+    STOPMIX=matrix_block('STOPMIX')
     SBOTMIX=matrix_block('SBOTMIX')
     STAUMIX=matrix_block('STAUMIX')
     NMNMIX=matrix_block('NMNMIX')
@@ -140,3 +139,6 @@ class SLHA():
     GUTAU=matrix_block('GUTAU')
     GUTAD=matrix_block('GUTAD')
     GUTAE=matrix_block('GUTAE')
+    
+
+
