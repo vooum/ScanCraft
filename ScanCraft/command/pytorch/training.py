0c1b6b486809cf79c979dfb6fb1d8f6f5deea445
Heyangle@Y410P <vooum@qq.com>
2018-01-17 03:12:37 +0800

    0117 add calculate file

--- ScanCraft/command/pytorch/training.py
+++ ScanCraft/command/pytorch/training.py
@@ -6,11 +6,14 @@ from ..format.data_container import capsule
 from ..scan.scan import scan
 
 class Training():
-    def __init__(self,mold,model=None):
-        if type(model) is str:
-            self.Classify=torch.load(model)
-        elif type(model) is Classify:
+    def __init__(self,mold,classify_model=None,calculate_model=None):
+        if type(classify_model) is str:
+            self.Classify=torch.load(classify_model)
+        elif type(classify_model) is Classify:
             self.Classify=model
+        if type(calculate_model) is str:
+            self.Calculate=torch.load(calculate_model)
+        elif type(calculate_model) is 
         self.mold=mold
         self.data_range=GetRanges(mold.free_parameter_list)
     def AddData(self,input_points,spectrums):
