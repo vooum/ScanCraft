47075ca2dbbc5d60478f2620834fa1520363f2cf
ji-ren <645665975@qq.com>
2019-06-15 14:21:06 +0800

    暂时想到的内容

--- ScanCraft/command/nexus/package.py
+++ ScanCraft/command/nexus/package.py
@@ -2,11 +2,28 @@
 
 class package(object):
     def __init__(self
-                ,package_name
-                ,package_dir
-                ,run_path
-                ,main_routine
+                ,package_name           #程序包的名字
+                ,package_dir            #程序包所在的目录
+                ,run_dir                #可执行文件的目录
+                ,main_routine           #运行命令
+                ,data_dir               #保留数据的目录
+                ,model_dir              #模型目录
+                ,input_dir              #输入文件所在的目录
+                ,input_file             #输入文件
+                ,output_dir             #输出文件的目录
+                ,record_dir             #保留输出文件的目录
+                ,out_file               #输出文件
                 ):
         self.package_dir=package_dir
-        self.run_dir
-        
+        self.run_dir=run_dir
+
+
+
+
+    def Run(self):
+        pass
+
+
+    def Record(self):
+        pass
+
