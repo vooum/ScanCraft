214d6ea21b71d4d60180d3fc47dbafefca0ff184
He Yangle <vooum@qq.com>
2019-07-17 18:49:11 +0800

    newSLHA-half

---
+++ ScanCraft/command/DataProcessing/SLHA/SLHA_document.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+class SLHA_line:
+    '''line in SLHA format'''
+    
+
+class SLHA_block:
+    '''block'''
+    def __init__(self,block_name):
+
+class SLHA_decay:
+    '''particals' decay information'''
+    def __init__(self,particle_code):
+
+class SLHA_document:
+    '''extracted messages from SLHA file'''
+    def __init__(self,SLHA_file):
+        with open(SLHA_file,'r') as SLHA:
+            self.text=SLHA.readlines()
+        self.blocks={}
+        block_name='head' # to store messages before first block
+        for line in self.text:
+            if 
