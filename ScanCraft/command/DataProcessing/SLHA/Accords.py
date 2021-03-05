88a904c49c55141650284c160b296c534f461ed9
He Yangle (R6 ubuntu) <vooum@qq.com>
2021-03-05 20:18:16 +0800

    more accords

--- ScanCraft/command/DataProcessing/SLHA/Accords.py
+++ ScanCraft/command/DataProcessing/SLHA/Accords.py
@@ -17,6 +17,7 @@ PDG = {
 }
 
 # scalar blocks
+## format: { block_name: { entry_code : definition}, }
 SLHA = {
     'SPINFO': {
         1: 'spectrum calculator name',
@@ -107,14 +108,25 @@ SLHA = {
         38: 'ggf->H3->gammagamma',
         310: 'ggf->H3->invisible',
     },
-    'LHCFIT': {
-        1: 'Hgammagamma', 2: 'Hff', 3: 'HVV'
-    },
     'ALPHA': {1: 'angle alpha'},
 }
 
-MicrOMEGAs = {
-    'RDINFO':{1:'Dark matter package', 2:'Version number'},
+NMSSMTools = {
+    # low energy tests
+    'LOWEN': {
+        1: 'BR(b->s_gamma)', 11: 'BR(b->s_gamma+Theor_Err)', 12: 'BR(b->s_gamma-Theor_Err)',
+        2: 'DeltaM_d_ps^-1', 21: 'DeltaM_d+Theor_Err', 22: 'DeltaM_d-Theor_Err',
+        3: 'DetaM_s', 31: 'DeltaM_s+Theor_Err', 32: 'DeltaM_s-Theor_Err',
+        4: 'BR(Bs->mu+mu-)', 41: 'BR(Bs->mu+mu-)+Theor_Err', 42: 'BR(Bs->mu+mu-)-Theor_Err',
+        5: 'BR(B+->tau+nu_tau)', 51: 'BR(B+->tau+nu_tau)+Theor_Err', 52: 'BR(B+->tau+nu_tau)-Theor_Err',
+        6: 'Del_a_mu', 61: 'Del_a_mu+Theor_Err', 62: 'Del_a_mu-Theor_Err'
+    },
+    'LHCFIT': {
+        1: 'Hgammagamma', 2: 'Hff', 3: 'HVV'
+    },
+
+    # MicrOMEGAs
+    'RDINFO': {1: 'Dark matter package', 2: 'Version number'},
     'ABUNDANCE': {
         1: 'T_f[GeV]',
         3: 'vSigma',
@@ -134,8 +146,8 @@ MicrOMEGAs = {
         3: 'csPsd',
         4: 'csNsd',
     },
-    'ANNIHILATION':{
-        0: 'sigmaV', #[cm^3/s]
+    'ANNIHILATION': {  # [cm^3/s]
+        0: 'sigmaV',
     }
 }
 
